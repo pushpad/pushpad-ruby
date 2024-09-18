a04eaafff9dc9633c8178ff77ac2b777ad2265fc
Marco Colli <collimarco91@gmail.com>
2024-09-18 16:55:30 +0200

    Refactor subscription

--- lib/pushpad/subscription.rb
+++ lib/pushpad/subscription.rb
@@ -4,49 +4,30 @@ module Pushpad
     end
 
     def self.count(options = {})
-      CountQuery.new(options).perform
+      project_id = options[:project_id] || Pushpad.project_id
+      raise "You must set project_id" unless project_id
+
+      endpoint = "https://pushpad.xyz/api/v1/projects/#{project_id}/subscriptions"
+      response = Request.head(endpoint, query_parameters: query_parameters(options))
+
+      unless response.code == "200"
+        raise CountError, "Response #{response.code} #{response.message}: #{response.body}"
+      end
+
+      response["X-Total-Count"].to_i
     end
 
-    class CountQuery
-      attr_reader :options
+    private
 
-      def initialize(options)
-        @options = options
-      end
-
-      def perform
-        project_id = options[:project_id] || Pushpad.project_id
-        raise "You must set project_id" unless project_id
-
-        endpoint = "https://pushpad.xyz/api/v1/projects/#{project_id}/subscriptions"
-        response = Request.head(endpoint, query_parameters: query_parameters)
-
-        unless response.code == "200"
-          raise CountError, "Response #{response.code} #{response.message}: #{response.body}"
-        end
-
-        response["X-Total-Count"].to_i
-      end
-
-      private
-
-      def query_parameters
-        [uid_query_parameters, tag_query_parameters].flatten(1)
-      end
-
-      def uid_query_parameters
-        options.fetch(:uids, []).map { |uid| ["uids[]", uid] }
-      end
-
-      def tag_query_parameters
-        tags = options.fetch(:tags, [])
-
-        if tags.is_a?(String)
-          [["tags", tags]]
-        else
-          tags.map { |tag| ["tags[]", tag] }
-        end
-      end
+    def self.query_parameters(options)
+      uids = options.fetch(:uids, [])
+      uids_query = uids.map { |uid| ["uids[]", uid] }
+      
+      tags = options.fetch(:tags, [])
+      tags_query = tags.is_a?(String) ? [["tags", tags]] : tags.map { |tag| ["tags[]", tag] }
+      
+      [uids_query, tags_query].flatten(1)
     end
+
   end
 end
